89f034a718311dc2e092246064b54b232d83785e
franklin-oks <franklinokeke2016@gmail.com>
2025-07-18 09:37:54 +0100

    Moved Firebase config to .env file

--- src/firebase.js
+++ src/firebase.js
@@ -10,14 +10,24 @@ import {
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { toast } from "react-toastify";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAs8TSl_iRqAdEMyCbQmXibjK2Ta3BpKfo",
+//   authDomain: "netflix-clone-f44a5.firebaseapp.com",
+//   projectId: "netflix-clone-f44a5",
+//   storageBucket: "netflix-clone-f44a5.firebasestorage.app",
+//   messagingSenderId: "918762471991",
+//   appId: "1:918762471991:web:05a4bee33646b39463b5b2",
+//   measurementId: "G-Q46K5514F2",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAs8TSl_iRqAdEMyCbQmXibjK2Ta3BpKfo",
-  authDomain: "netflix-clone-f44a5.firebaseapp.com",
-  projectId: "netflix-clone-f44a5",
-  storageBucket: "netflix-clone-f44a5.firebasestorage.app",
-  messagingSenderId: "918762471991",
-  appId: "1:918762471991:web:05a4bee33646b39463b5b2",
-  measurementId: "G-Q46K5514F2",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
